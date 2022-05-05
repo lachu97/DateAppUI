2bc9152568c96b78324a71b7491acca397af7c48
Lakshu Narasu <ced15i004@iiitdm.ac.in>
2022-05-05 07:18:41 +0530

    With Login and SignUp Ui and Splash Screen

--- app/src/main/java/com/betelguese/dateappui/MainActivity.kt
+++ app/src/main/java/com/betelguese/dateappui/MainActivity.kt
@@ -16,12 +16,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.betelguese.dateappui.ui.theme.DateAppUITheme
 import com.betelguese.dateappui.viewUI.Interests
 import com.betelguese.dateappui.viewUI.Rowlisty
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             DateAppUITheme {
